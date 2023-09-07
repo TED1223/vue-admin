9be0ce9400abc6fbf4b4376f25f4b4a587a58a43
pengfeng <1114607968@qq.com>
2023-09-07 16:37:28 +0800

    feat: init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <template>
-  <div></div>
+  <div>
+    <h1>Hello</h1>
+    <el-button type="primary" size="default" :icon="Plus"> ssadasd</el-button>
+    <el-button type="primary" size="default" :icon="Edit"> ssadasd</el-button>
+    <el-button type="primary" size="default" :icon="Delete"> ssadasd</el-button>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Plus, Edit, Delete } from "@element-plus/icons-vue";
+</script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.div {
+  .h1 {
+    color: $color;
+  }
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
+// main.ts
 import { createApp } from "vue";
-import "./style.css";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
 import App from "./App.vue";
+import zhCn from "element-plus/dist/locale/zh-cn.mjs";
+import "virtual:svg-icons-register";
+import gloablComponent from "./components/index";
+import "@/styles/index.scss";
 
-createApp(App).mount("#app");
+//获取应用实例
+const app = createApp(App);
+//安装ElementPlus
+app.use(ElementPlus, {
+  locale: zhCn,
+});
+app.use(gloablComponent);
+//将应用挂载到挂载点
+app.mount("#app");
