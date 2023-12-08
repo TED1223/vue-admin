18e7f72b65c8890a51fdfe4543b78f58235788d7
pengfeng <1114607968@qq.com>
2023-12-08 16:00:25 +0800

    feat: 6.12 删除按钮&&清空数据

--- src/views/product/attr/index.vue
+++ src/views/product/attr/index.vue
@@ -36,8 +36,25 @@
         </el-table-column>
         <el-table-column label="操作" width="120px">
           <template #="{ row, $index }">
-            <el-button type="primary" size="small" icon="Edit"></el-button>
-            <el-button type="primary" size="small" icon="Delete"></el-button>
+            <el-button
+              type="primary"
+              size="small"
+              icon="Edit"
+              @click="updateAttr(row)"
+            ></el-button>
+            <el-popconfirm
+              :title="`你确定删除${row.attrName}?`"
+              width="200px"
+              @confirm="deleteAttr(row.id)"
+            >
+              <template #reference>
+                <el-button
+                  type="primary"
+                  size="small"
+                  icon="Delete"
+                ></el-button>
+              </template>
+            </el-popconfirm>
           </template>
         </el-table-column>
       </el-table>
@@ -84,7 +101,16 @@
             <div v-else @click="toEdit(row, $index)">{{ row.valueName }}</div>
           </template>
         </el-table-column>
-        <el-table-column label="属性值操作"></el-table-column>
+        <el-table-column label="属性值操作">
+          <template #="{ row, $index }">
+            <el-button
+              type="primary"
+              size="small"
+              icon="Delete"
+              @click="attrParams.attrValueList.splice($index, 1)"
+            ></el-button>
+          </template>
+        </el-table-column>
       </el-table>
       <el-button type="primary" size="default" icon="Plus" @click="save"
         >保存</el-button
@@ -96,10 +122,11 @@
 
 <script setup lang="ts">
 import useCategoryStore from "@/store/modules/category";
-import { nextTick, reactive, ref, watch } from "vue";
-import { reqAddOrUpdateAttr, reqAttr } from "@/api/product/attr";
+import { nextTick, onBeforeMount, reactive, ref, watch } from "vue";
+import { reqAddOrUpdateAttr, reqAttr, reqRemoveAttr } from "@/api/product/attr";
 import { Attr, AttrResponseData, AttrValue } from "@/api/product/attr/type";
 import { ElMessage } from "element-plus";
+import { ResponseData } from "@/api/type";
 
 let categoryStore = useCategoryStore();
 
@@ -129,6 +156,41 @@ watch(
     getAttr();
   },
 );
+
+//路由组件销毁的时候，把仓库分类相关的数据清空
+onBeforeMount(() => {
+  //清空仓库的数据
+  categoryStore.$reset();
+});
+
+//删除某一个已有的属性方法回调
+const deleteAttr = async (attrId: number) => {
+  //发相应的删除已有的属性的请求
+  let result: ResponseData = await reqRemoveAttr(attrId);
+  //删除成功
+  if (result.code === 200) {
+    ElMessage({
+      type: "success",
+      message: "删除成功",
+    });
+    //获取一次已有的属性与属性值
+    getAttr();
+  } else {
+    ElMessage({
+      type: "error",
+      message: "删除失败",
+    });
+  }
+};
+
+//table表格修改已有属性按钮的回调
+const updateAttr = (data: Attr) => {
+  //切换为添加与修改属性的结构
+  scene.value = 1;
+  //将已有的属性对象赋值给attrParams对象即为
+  //ES6->Object.assign进行对象的合并
+  Object.assign(attrParams, JSON.parse(JSON.stringify(data)));
+};
 
 //属性值表单元素失却焦点事件回调
 const toLook = (row: AttrValue, $index: number) => {
@@ -182,6 +244,9 @@ const save = async () => {
       type: "success",
       message: attrParams.id ? "修改成功" : "添加成功",
     });
+
+    //获取一次已有的属性与属性值
+    getAttr();
   } else {
     ElMessage({
       type: "error",
